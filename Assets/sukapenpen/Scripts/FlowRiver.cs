29591a0d4eae02dac4d98880ad847ef024797a6a
sukapenpen <ne280194@senshu-u.jp>
2019-03-03 18:52:50 +0900

    川と橋ができた

---
+++ Assets/sukapenpen/Scripts/FlowRiver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowRiver : MonoBehaviour
+{
+	private float riverSpeed = -0.05f;
+	
+	private void Update ()
+	{
+		transform.Translate (0, riverSpeed, 0);
+		if (transform.position.z < -6.5f)
+		{
+			transform.position = new Vector3 (0, 0, 0);
+		}
+	}
+}

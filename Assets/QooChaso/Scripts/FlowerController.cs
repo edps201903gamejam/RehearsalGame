93d9eba9befdd9f080cd56ee7d35743a8f76a914
QooChaso <qoochaso238@gmail.com>
2019-03-04 02:36:09 +0900

    タイトル、リザルト、悪魔以外

---
+++ Assets/QooChaso/Scripts/FlowerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class FlowerController : MonoBehaviour
+{
+    public GameObject flower;
+    private GameObject[] flowerList;
+    private int finCount = 0;
+
+    void Start()
+    {
+        Instantiate(flower, new Vector3(-4.2f, 1.0f, 4.2f), Quaternion.Euler(35f, 0, 0));
+        Instantiate(flower, new Vector3(4.4f, 1.0f, -4.0f), Quaternion.Euler(55f, 0, 0));
+        Instantiate(flower, new Vector3(-4.2f, 1.0f, -4.0f), Quaternion.Euler(55f, 0, 0));
+    }
+
+    void Update(){ CheckFin(); }
+
+    private void CheckFin()
+    {
+        flowerList = GameObject.FindGameObjectsWithTag("plant");
+        foreach (GameObject flower in flowerList)
+        {
+            Flower flo = flower.GetComponent<Flower>();
+            if (flo.IsGrowed || flo.IsWithered) { finCount++; }
+        }
+        if (finCount == flowerList.Length) { toResult(); }
+        else finCount = 0;
+    }
+
+    private void toResult(){ Debug.Log("おわり"); }
+
+}
--- Assets/QooChaso/Scripts/PlayerController.cs
+++ Assets/QooChaso/Scripts/PlayerController.cs
@@ -2,41 +2,30 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    public WateringCan hoge;
 
-    private Rigidbody rb;
-    private WateringCan zyouro;
+    void Start(){ hoge = new WateringCan(); }
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        zyouro = new WateringCan();
-
-    }
-
-    void Update()
-    {
-        moveCOntrol();
-
-    }
-
-    private void moveCOntrol(){
-        // カーソルキーの入力を取得
-        var moveHorizontal = Input.GetAxis("Horizontal");
-        var moveVertical = Input.GetAxis("Vertical");
-
-        // カーソルキーの入力に合わせて移動方向を設定
-        var movement = new Vector3(moveHorizontal, 0, moveVertical);
-
-        // Ridigbody に力を与えて玉を動かす
-        rb.AddForce(movement * speed);
-    }
-
-    void OnCollisionStay(Collision other)
+    void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "plant")
         {
-            Destroy(other.gameObject);
+            if (Input.GetKeyDown(KeyCode.Space) && hoge.isWaterd)
+            {
+                hoge.RemovedWater();
+                other.GetComponent<Flower>().minusWhtherPercentage();
+                Debug.Log("水をあげた");
+            }
+        }
+
+        if (other.gameObject.tag == "water")
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !hoge.isWaterd)
+            {
+                hoge.ToMaxWaterGase();
+                Debug.Log("水をくんだ");
+            }
         }
     }
+
 }

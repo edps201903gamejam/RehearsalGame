6065ea836d63d3a4d0f147cb91864b9006a0d61e
flanny7 <mottei.senshu@gmail.com>
2019-03-06 00:33:02 +0900

    フォルダ構成の整理

---
+++ Assets/Scripts/QooChaso/WateringCan.cs
@@ -0,0 +1,22 @@
+﻿// Writer：QooChaso
+
+public class WateringCan
+{
+
+    public bool isWaterd;
+
+    public WateringCan()
+    {
+        isWaterd = false;
+    }
+
+    public void ToMaxWaterGase()
+    {
+        isWaterd = true;
+    }
+
+    public void RemovedWater()
+    {
+        isWaterd = false;
+    }
+}
---
+++ Assets/Scripts/flanny7/FyUtility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public static class FyUtility
+{
+
+    /// <summary>
+    /// 対象がnullであるときにデバッグ用ログを出す関数
+    /// </summary>
+    /// <param name="_object">チェックする対象</param>
+    /// <param name="_gameObject">対象を持つGameObject</param>
+    public static void NullCheck(UnityEngine.Object _object, GameObject _gameObject = null)
+    {
+        if (_object == null)
+        {
+            FyUtility.LogError(_object.name + " is null.", _gameObject);
+        }
+    }
+
+    /// <summary>
+    /// エラーログをイイ感じに表示する
+    /// </summary>
+    /// <param name="_msg">ログに出力させる文字</param>
+    public static void LogError(string _msg, GameObject _gameObject = null)
+    {
+        Debug.LogError("<color=red>" + _msg + "</color>", _gameObject); Debug.Break();
+    }
+}

5ccb14d7b91170462db56b5e85b5542d896781df
flanny7 <mottei.senshu@gmail.com>
2019-03-06 17:19:06 +0900

    add ResultUIManager

---
+++ Assets/Scripts/flanny7/Result/ResultTester.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ResultTester : MonoBehaviour
+{
+    [System.Serializable]
+    private struct Param
+    {
+        public int FlowerCount;
+        public int DieCount;
+        public BadgeComponent.BADGE_TYPE BadgeType;
+    }
+
+    [SerializeField]
+    private KeyCode openKeyCode = KeyCode.Z;
+    [SerializeField]
+    private KeyCode closeKeyCode = KeyCode.X;
+    [SerializeField]
+    private ResultUIManager uIManager = null;
+    [SerializeField]
+    private Param param;
+
+    private void Start()
+    {
+        FyUtility.NullCheck(this.uIManager, this.gameObject);
+    }
+
+    private void Update ()
+    {
+        if (Input.GetKeyDown(this.openKeyCode)) { this.uIManager.Open(this.param.FlowerCount, this.param.DieCount, this.param.BadgeType); }
+        if (Input.GetKeyDown(this.closeKeyCode)) { this.uIManager.Close(); }
+    }
+}
---
+++ Assets/Scripts/flanny7/Result/ResultUIManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class ResultUIManager : MonoBehaviour
+{
+    private static readonly float FLOAT_MAX_VALUE = float.MaxValue;
+
+    [SerializeField]
+    private CanvasGroup canvasGroup_ = null;
+    [SerializeField]
+    private CounterComponent counter_ = null;
+    [SerializeField]
+    private BadgeComponent badge_ = null;
+    [SerializeField]
+    private float fadeInTime = 1.0f;
+    [SerializeField]
+    private float fadeOutTime = 1.0f;
+
+    private float elapsedTime = 0.0f;
+    private bool isActive = false;
+
+    private float fadeInStartTime = FLOAT_MAX_VALUE;
+    private bool isFadeIn { get { return (this.elapsedTime - this.fadeInStartTime) <= this.fadeInTime; } }
+
+    private float fadeOutStartTime = FLOAT_MAX_VALUE;
+    private bool isFadeOut { get { return (this.elapsedTime - this.fadeOutStartTime) <= this.fadeOutTime; } }
+
+    private void Start()
+    {
+        FyUtility.NullCheck(this.canvasGroup_, this.gameObject);
+        FyUtility.NullCheck(this.counter_, this.gameObject);
+        FyUtility.NullCheck(this.badge_, this.gameObject);
+
+        this.elapsedTime = 0.0f;
+        this.isActive = false;
+        this.canvasGroup_.alpha = 0;
+        this.fadeInStartTime = FLOAT_MAX_VALUE;
+        this.fadeOutStartTime = FLOAT_MAX_VALUE;
+    }
+
+    private void Update()
+    {
+        this.elapsedTime += Time.deltaTime;
+        
+        if (this.isFadeIn)
+        {
+            this.canvasGroup_.alpha = Easer.EaseIn(this.elapsedTime - this.fadeInStartTime, this.fadeInTime);
+            if (1 <= this.canvasGroup_.alpha) { this.canvasGroup_.alpha = 1; this.fadeInStartTime = FLOAT_MAX_VALUE; }
+        }
+
+        if (this.isFadeOut)
+        {
+            this.canvasGroup_.alpha = Easer.EaseOut(this.elapsedTime - this.fadeOutStartTime, this.fadeOutTime);
+            if (this.canvasGroup_.alpha <= 0) { this.canvasGroup_.alpha = 0; this.fadeOutStartTime = FLOAT_MAX_VALUE; }
+        }
+    }
+
+    public void Open(int _flowerCount, int _dieCount, BadgeComponent.BADGE_TYPE _badgeType)
+    {
+        this.counter_.FlowerCount = _flowerCount;
+        this.counter_.DieCount = _dieCount;
+        this.badge_.BadgeType = _badgeType;
+
+        this.fadeInStartTime = this.elapsedTime;
+    }
+
+    public void Close()
+    {
+        this.fadeOutStartTime = this.elapsedTime;
+    }
+}

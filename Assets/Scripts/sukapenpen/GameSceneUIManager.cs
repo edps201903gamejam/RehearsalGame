6e2f585ad860f88f42cf3c44de98e13ea1a463ec
sukapenpen <ne280194@senshu-u.jp>
2019-03-08 10:07:59 +0900

    制限時間5秒前のカウントダウンができる

--- Assets/Scripts/sukapenpen/GameSceneUIManager.cs
+++ Assets/Scripts/sukapenpen/GameSceneUIManager.cs
@@ -8,11 +8,15 @@ public class GameSceneUIManager : SingletonMonoBehaviour<GameSceneUIManager>
 	private enum GameUIState
 	{
 		Set,
-		Start
+		Start,
+		Result
 	}
 
+	[SerializeField] private Sprite[] startStopCountImages;
 	[SerializeField]
-	private Sprite[] startCountImages;
+	private Sprite startImage;
+	[SerializeField]
+	private Sprite stopImage;
 	
 	[SerializeField]
 	private float maxStartTime = 4;
@@ -22,31 +26,47 @@ public class GameSceneUIManager : SingletonMonoBehaviour<GameSceneUIManager>
 	
 	private void Start ()
 	{
-		startCountImage = GameObject.Find("StartCounter").GetComponent<Image> ();
+		startCountImage = GameObject.Find("StartStopCounter").GetComponent<Image> ();
+		startStopCountImages[0] = startImage;
 		this.countTime = 0;
 		gameUIState = GameUIState.Set;
 	}
 	
 	public void OnUpdate (float _timeLimit)
 	{
-		int startSec = (int) _timeLimit;
+		int sec = (int) _timeLimit;
 		switch (gameUIState)
 		{
 			case GameUIState.Set:
-				if (startSec >= 0)
+				if (sec >= 0)
 				{
-					startCountImage.sprite = startCountImages[startSec];
+					startCountImage.sprite = startStopCountImages[sec];
 				}
 				else
 				{
 					startCountImage.enabled = false;
 					gameUIState = GameUIState.Start;
+					startStopCountImages[0] = stopImage;
 				}
 
 				break;
 			
 			case GameUIState.Start:
-				Debug.Log("ゲーム中のUI処理");
+				Debug.Log("ゲーム中のUI処理" + sec);
+				if (sec < 0)
+				{
+					startCountImage.enabled = false;
+					gameUIState = GameUIState.Result;
+				}
+				else if(sec < 6)
+				{
+					startCountImage.enabled = true;
+					startCountImage.sprite = startStopCountImages[sec];
+				}
+				break;
+			
+			case GameUIState.Result:
+				Debug.Log("リザルトの処理");
 				break;
 		}
 	}
